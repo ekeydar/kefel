f9f8fff469b597acaabcb97aa7bbe9dfbaa548d5
Eran Keydar <ekeydar@gmail.com>
2015-07-05 14:48:36 +0300

    wip

--- kefel/files/app/js/kefel.js
+++ kefel/files/app/js/kefel.js
@@ -1,7 +1,7 @@
 var app = angular.module('kefel_app', ['my.filters']);
 
-app.controller('KefelController', ['$scope',
-function($scope) {
+app.controller('KefelController', ['$scope','$document',
+function($scope,$document) {
     $scope.rows = [[7,8,9],
                    [4,5,6],
                    [1,2,3],
@@ -9,6 +9,30 @@ function($scope) {
     $scope.input = {
         result: []
     }
+    $scope.keyPress = function() {
+        console.log("key press");
+    }
+    $scope.addDigit = function(d) {
+        $scope.input.result.push(d);
+    }
+    $scope.backspace = function() {
+        $scope.input.result.pop();
+    }
+    $scope.reset = function() {
+        $scope.input.result = [];
+    }
+    $document.on('keydown',function(ev) {
+        $scope.$apply(function() {
+            if (!isNaN(parseInt(ev.key))) {
+                $scope.addDigit(ev.key);
+            } else if (ev.keyCode == 8 || ev.keyCode == 46) {
+                $scope.backspace();
+            } else if (ev.keyCode == 27) {
+                $scope.reset();
+            }
+
+        });
+    });
 }]);
 
 
@@ -22,11 +46,11 @@ function($scope) {
     }
     $scope.clicked = function() {
         if ($scope.isBack()) {
-            $scope.input.result.pop();
+            $scope.backspace();
         } else if ($scope.isClear()) {
-            $scope.input.result = [];
+            $scope.reset();
         } else {
-            $scope.input.result.push($scope.key);
+            $scope.addDigit($scope.key);
         }
     }
 }]);
