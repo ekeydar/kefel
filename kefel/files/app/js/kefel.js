3e659e94e81e9cd53549a3013685ec6610086786
Eran Keydar <ekeydar@gmail.com>
2015-07-06 13:41:46 +0300

    wip

--- kefel/files/app/js/kefel.js
+++ kefel/files/app/js/kefel.js
@@ -1,35 +1,93 @@
 var app = angular.module('kefel_app', ['my.filters']);
 
-function Question(index) {
-    Question.prototype.genNum = function() {
-	return Math.ceil(Math.random(1)*10)
-    }
-    Question.prototype.NOTASKED = 0;
-    Question.prototype.RIGHT = 1;
-    Question.prototype.WRONG = 2;
-    this.num1 = this.genNum();
-    this.num2 = this.genNum();
-    this.answerLength = (this.num1*this.num2).toString().length;
-    this.index = index;
-    this.status = this.NOTASKED;
-    this.input = [];
-    Question.prototype.pushToInput = function(i) {
-        if (this.input.length < this.answerLength) {
-            this.input.push(i);
-        }
-    }
-    Question.prototype.popInput = function() {
-        if (this.input.length > 0) {
-            this.input.pop();
-        }
-    }
-    Question.prototype.resetInput = function() {
-        this.input = [];
-    }
-}
+app.controller('KefelController', ['$scope','$document','$filter',
+function($scope,$document,$filter) {
 
-app.controller('KefelController', ['$scope','$document',
-function($scope,$document) {
+    function Question(index) {
+        Question.prototype.genNum = function() {
+        return Math.ceil(Math.random(1)*10)
+        }
+        Question.prototype.NOTASKED = 0;
+        Question.prototype.RIGHT = 1;
+        Question.prototype.WRONG = 2;
+        this.num1 = this.genNum();
+        this.num2 = this.genNum();
+        this.answerLength = (this.num1*this.num2).toString().length;
+        this.index = index;
+        this.status = this.NOTASKED;
+        this.input = [];
+        Question.prototype.pushToInput = function(i) {
+            if (this.input.length < this.answerLength) {
+                this.input.push(i);
+            }
+        }
+        Question.prototype.popInput = function() {
+            if (this.input.length > 0) {
+                this.input.pop();
+            }
+        }
+        Question.prototype.resetInput = function() {
+            this.input = [];
+        }
+        Question.prototype.check = function() {
+            this.endTime = new Date().getTime();
+            this.totalTime = this.endTime - this.startTime;
+            console.log(this.endTime,this.startTime);
+            console.log(this.totalTime);
+            var userAnswer = parseInt($filter('joinList')(this.input));
+            if (userAnswer == this.num1*this.num2) {
+                this.status = this.RIGHT;
+            } else {
+                this.status = this.WRONG;
+            }
+        }
+        Question.prototype.start = function() {
+            this.startTime = new Date().getTime();
+        }
+    }
+
+    $scope.getRightCount = function() {
+        var rightCount = 0;
+        $scope.questions.forEach(function(q) {
+            if (q.status == q.RIGHT) {
+                rightCount++;
+            }
+        });
+        return rightCount;
+    }
+
+    $scope.getTotalTime = function() {
+        var result = 0.0;
+        $scope.questions.forEach(function(q) {
+            result+=q.totalTime;
+        }); 
+        return result / 1000.0;
+    }
+
+    $scope.getTotalRightTime = function() {
+        var result = 0.0;
+        $scope.questions.forEach(function(q) {
+            if (q.status == q.RIGHT) {
+                result+=q.totalTime;    
+            }
+        }); 
+        return result / 1000.0;
+    }
+
+    $scope.getTimeForRight = function() {
+        var rc = $scope.getRightCount();
+        if (rc > 0) { 
+            return $scope.getTotalRightTime() / rc;
+        } else {
+            return '';
+        }
+    }
+
+    $scope.getScore = function() {
+        return Math.ceil(100 * $scope.getRightCount() /$scope.questions.length); 
+    }
+
+
     $scope.rows = [[7,8,9],
                    [4,5,6],
                    [1,2,3],
@@ -37,12 +95,10 @@ function($scope,$document) {
 
     $scope.questions = [];
     $scope.curQuestion = null;
-    for (var i = 0 ; i < 10 ; i++) {
+    $scope.curQuestionIndex = 0;
+    $scope.done = false;
+    for (var i = 0 ; i < 3 ; i++) {
 	    $scope.questions.push(new Question(i));
-    }
-    $scope.curQuestion = $scope.questions[0];
-    $scope.input = {
-        result: []
     }
     $scope.addDigit = function(d) {
         if ($scope.curQuestion) {
@@ -60,7 +116,17 @@ function($scope,$document) {
         }
     }
     $scope.submit = function() {
-        console.log("submitted");
+        if ($scope.curQuestion) {
+            $scope.curQuestion.check();
+        }
+        $scope.curQuestionIndex++;
+        if ($scope.curQuestionIndex < $scope.questions.length) {
+            $scope.curQuestion = $scope.questions[$scope.curQuestionIndex];
+            $scope.curQuestion.start();
+        } else {
+            $scope.curQuestion = null;
+            $scope.done = true;
+        }
     }
     $document.on('keydown',function(ev) {
         $scope.$apply(function() {
@@ -75,6 +141,11 @@ function($scope,$document) {
             }
         });
     });
+    $scope.start = function() {
+        this.curQuestion = $scope.questions[0];
+        this.curQuestion.start();
+    }
+    $scope.start();
 }]);
 
 
