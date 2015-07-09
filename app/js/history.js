e34f0a0b3d738f8179b0ee6774ef3cdeb2664981
Eran Keydar <ekeydar@gmail.com>
2015-07-09 11:50:52 +0300

    history.js

--- app/js/history.js
+++ app/js/history.js
@@ -2,45 +2,63 @@
 var app = angular.module('kefel_app');
 
 app.controller('HistoryController',['$scope','$document','$location','UsersDB',
-    function($scope,$document,$location,UsersDB) {
-    	var scores = [];
-    	var curUser = UsersDB.getCurUser();
-    	var results = curUser.results || [];
-    	var scores = [];
-    	var dates = [];
-    	results.forEach(function(r) {
-    		scores.push(r.score);
-    		var dt = new Date(r.timestamp);
-    		var dtStr = '' + (dt.getMonth() + 1) + '/' + dt.getDate()
-    		dates.push(dtStr);
-    	});
-    	console.log(scores);
-    	$scope.histConfig = {
-        	options: {
-            	chart: {
-                	type: 'line'
-            	}
-        	},
-        	yAxis: {
-          		title: {
-                	text: 'ניקוד',
-                },
-                min: 0,
-                max: 100,
-            },
-            xAxis: {
-            	title: {
-            		text: 'תאריך',
-            	},
-            	categories: dates,
-        	},
+	function($scope,$document,$location,UsersDB) {
+		var scores = [];
+		var curUser = UsersDB.getCurUser();
+		var results = curUser.results || [];
+		var scores = [];
+		var avgTimes = [];
+		var dates = [];
+		results.forEach(function(r) {
+			scores.push(r.score);
+			var dt = new Date(r.timestamp);
+			var dtStr = '' + (dt.getMonth() + 1) + '/' + dt.getDate()
+			dates.push(dtStr);
+			avgTimes.push(r.timeForRight);
+		});
+		console.log(scores);
+		$scope.histConfig = {
+			options: {
+				chart: {
+					type: 'line'
+				}
+			},
+        	yAxis: [
+        		{ // Primary yAxis
+        			title: {
+        				text: 'ניקוד',
+        				style: {
+        					color: Highcharts.getOptions().colors[1]
+        				}
+        			}
+        		}, 
+        		{ // secondary yAxis
+        			title: {
+        				text: 'זמן שלאלה נכונה',
+        				style: {
+        					color: Highcharts.getOptions().colors[0]
+        				}
+        			},
+        		},
+        	],
         	series: [{
-            	data: scores,
+        		name: 'Rainfall',
+        		data: scores,
+
+        	}, {
+        		name: 'Temperature',
+        		data: avgTimes,
         	}],
-        	title: {
-            	text: 'ההיסטוריה שלי'
-        	},
-        	loading: false
-    	}
-    }
+			xAxis: {
+				title: {
+					text: 'תאריך',
+				},
+				categories: dates,
+			},
+			title: {
+				text: 'ההיסטוריה שלי'
+			},
+			loading: false
+		}
+}
 ]);
