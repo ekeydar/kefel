45adec46961d2e65ed79699420b6923f72dfc7e5
Eran Keydar <ekeydar@gmail.com>
2015-07-09 18:43:39 +0300

    history.js

--- app/js/history.js
+++ app/js/history.js
@@ -3,6 +3,9 @@ var app = angular.module('kefel_app');
 
 app.controller('HistoryController',['$scope','$document','$location','UsersDB',
 	function($scope,$document,$location,UsersDB) {
+		$scope.frac2 =  function(num) {
+			return Math.ceil(num*100) / 100;
+		}
 		var scores = [];
 		var curUser = UsersDB.getCurUser();
 		var results = curUser.results || [];
@@ -14,17 +17,24 @@ app.controller('HistoryController',['$scope','$document','$location','UsersDB',
 			var dt = new Date(r.timestamp);
 			var dtStr = '' + (dt.getMonth() + 1) + '/' + dt.getDate()
 			dates.push(dtStr);
-			avgTimes.push(r.timeForRight);
+			avgTimes.push($scope.frac2(r.timeForRight));
 		});
 		console.log(scores);
 		$scope.histConfig = {
-			options: {
-				chart: {
-					type: 'line'
-				}
-			},
+			chart: {
+            	zoomType: 'xy'
+        	},
+			legend: {
+            	layout: 'vertical',
+            	align: 'left',
+            	x: 120,
+            	verticalAlign: 'top',
+            	y: 100,
+            	floating: false,
+            	backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+        	},
         	yAxis: [
-        		{ // Primary yAxis
+        		{ 
         			title: {
         				text: 'ניקוד',
         				style: {
@@ -34,26 +44,34 @@ app.controller('HistoryController',['$scope','$document','$location','UsersDB',
         		}, 
         		{ // secondary yAxis
         			title: {
-        				text: 'זמן שלאלה נכונה',
+        				text: 'זמן לתשובה נכונה',
         				style: {
         					color: Highcharts.getOptions().colors[0]
         				}
         			},
+        			opposite: true,
         		},
         	],
+        	tooltip: {
+          	  	shared: true
+        	},
         	series: [{
-        		name: 'Rainfall',
+        		name: 'ניקוד',
         		data: scores,
+        		min: 0,
+        		max: 100,
 
         	}, {
-        		name: 'Temperature',
+        		name: 'זמן לתשובה נכונה',
         		data: avgTimes,
+        		yAxis: 1,
         	}],
 			xAxis: {
 				title: {
 					text: 'תאריך',
 				},
 				categories: dates,
+				crosshair: true,
 			},
 			title: {
 				text: 'ההיסטוריה שלי'
